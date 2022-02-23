18bf4e0d490078d6c40ec50f3dab4a4724f13fff
DEVARAPUSAIGANESH <99487691+DEVARAPUSAIGANESH@users.noreply.github.com>
2022-02-23 22:48:46 +0530

    Create Listofmax.py

---
+++ Listofmax.py
@@ -0,0 +1,14 @@
+n=int(input())
+l=[]
+for i in range(n):
+    x=int(input())
+    l.append(x)
+print('the minimum number is:',min(l))
+print('the maximum number is:',max(l))
+
+Output
+2
+35
+42
+the minimum number is: 35
+the maximum number is: 42
